ceece947a9ea243ba252dcdc6cfdc6bba42a730e
maxikg <maxmax58@googlemail.com>
2015-11-19 13:49:06 +0100

    Outsource database adapter.

---
+++ src/main/java/de/maxikg/mongowg/RegionStorageAdapter.java
@@ -0,0 +1,127 @@
+package de.maxikg.mongowg;
+
+import com.google.common.base.Preconditions;
+import com.google.common.collect.HashBasedTable;
+import com.google.common.collect.ImmutableSet;
+import com.google.common.collect.MapMaker;
+import com.google.common.collect.Table;
+import com.mongodb.Block;
+import com.mongodb.async.client.MongoCollection;
+import com.mongodb.async.client.MongoDatabase;
+import com.mongodb.client.model.Filters;
+import com.mongodb.client.model.UpdateOptions;
+import com.mongodb.client.result.DeleteResult;
+import com.mongodb.client.result.UpdateResult;
+import com.sk89q.worldguard.protection.managers.RegionDifference;
+import com.sk89q.worldguard.protection.managers.storage.RegionDatabaseUtils;
+import com.sk89q.worldguard.protection.managers.storage.StorageException;
+import com.sk89q.worldguard.protection.regions.ProtectedRegion;
+import de.maxikg.mongowg.model.ProcessingProtectedRegion;
+import de.maxikg.mongowg.utils.ConcurrentUtils;
+import de.maxikg.mongowg.utils.OperationResultCallback;
+import org.bson.Document;
+import org.bson.types.ObjectId;
+
+import java.util.Set;
+import java.util.concurrent.ConcurrentMap;
+import java.util.concurrent.CountDownLatch;
+import java.util.concurrent.atomic.AtomicReference;
+
+public class RegionStorageAdapter {
+
+    public static final String COLLECTION_NAME = "regions";
+
+    private final Table<String, String, ObjectId> databaseIds = HashBasedTable.create();
+    private final MongoDatabase database;
+
+    public RegionStorageAdapter(MongoDatabase database) {
+        this.database = Preconditions.checkNotNull(database, "database must be not null.");
+    }
+
+    public ObjectId resolveObjectId(String world, ProtectedRegion region) {
+        return resolveObjectId(world, region.getId());
+    }
+
+    public ObjectId resolveObjectId(String world, String id) {
+        return databaseIds.get(world, id);
+    }
+
+    public Set<ProtectedRegion> loadAll(final String world) throws StorageException {
+        final CountDownLatch waiter = new CountDownLatch(1);
+        final AtomicReference<Throwable> lastError = new AtomicReference<>();
+        final ConcurrentMap<String, ProtectedRegion> regions = new MapMaker().makeMap();
+        final ConcurrentMap<ProtectedRegion, String> parents = new MapMaker().makeMap();
+        getCollection().find(Filters.eq("world", world)).forEach(
+                new Block<ProcessingProtectedRegion>() {
+                    @Override
+                    public void apply(ProcessingProtectedRegion region) {
+                        if (!world.equals(region.getWorld()))
+                            return;
+                        ProtectedRegion protectedRegion = region.getRegion();
+                        databaseIds.put(world, protectedRegion.getId(), region.getDatabaseId());
+                        regions.putIfAbsent(protectedRegion.getId(), protectedRegion);
+                        String parent = region.getParent();
+                        if (parent != null)
+                            parents.putIfAbsent(protectedRegion, parent);
+                    }
+                },
+                OperationResultCallback.<Void>create(lastError, waiter)
+        );
+        ConcurrentUtils.safeAwait(waiter);
+        Throwable realLastError = lastError.get();
+        if (realLastError != null)
+            throw new StorageException("An error occurred while saving or updating in MongoDB.", realLastError);
+        RegionDatabaseUtils.relinkParents(regions, parents);
+        return ImmutableSet.copyOf(regions.values());
+    }
+
+    public void saveAll(final String world, Set<ProtectedRegion> set) throws StorageException {
+        MongoCollection<ProcessingProtectedRegion> collection = getCollection();
+        final AtomicReference<Throwable> lastError = new AtomicReference<>();
+        final CountDownLatch waiter = new CountDownLatch(set.size());
+        for (ProtectedRegion region : set) {
+            collection.updateOne(
+                    Filters.and(Filters.eq("name", region.getId()), Filters.eq("world", world)),
+                    new Document("$set", new ProcessingProtectedRegion(region, world)),
+                    new UpdateOptions().upsert(true),
+                    OperationResultCallback.<UpdateResult>create(lastError, waiter)
+            );
+        }
+        ConcurrentUtils.safeAwait(waiter);
+        Throwable realLastError = lastError.get();
+        if (realLastError != null)
+            throw new StorageException("An error occurred while saving or updating in MongoDB.", realLastError);
+    }
+
+    public void saveChanges(final String world, RegionDifference regionDifference) throws StorageException {
+        MongoCollection<ProcessingProtectedRegion> collection = getCollection();
+        Set<ProtectedRegion> changed = regionDifference.getChanged();
+        Set<ProtectedRegion> removed = regionDifference.getRemoved();
+        final AtomicReference<Throwable> lastError = new AtomicReference<>();
+        final CountDownLatch waiter = new CountDownLatch(changed.size() + removed.size());
+        for (ProtectedRegion region : regionDifference.getChanged()) {
+            collection.updateOne(
+                    Filters.and(Filters.eq("name", region.getId()), Filters.eq("world", world)),
+                    new Document("$set", new ProcessingProtectedRegion(region, world)),
+                    new UpdateOptions().upsert(true),
+                    OperationResultCallback.<UpdateResult>create(lastError, waiter)
+            );
+        }
+
+        for (ProtectedRegion region : regionDifference.getRemoved()) {
+            collection.deleteOne(
+                    Filters.and(Filters.eq("name", region.getId()), Filters.eq("world", world)),
+                    OperationResultCallback.<DeleteResult>create(lastError, waiter)
+            );
+        }
+
+        ConcurrentUtils.safeAwait(waiter);
+        Throwable realLastError = lastError.get();
+        if (realLastError != null)
+            throw new StorageException("An error occurred while saving or updating in MongoDB.", realLastError);
+    }
+
+    private MongoCollection<ProcessingProtectedRegion> getCollection() {
+        return database.getCollection(COLLECTION_NAME, ProcessingProtectedRegion.class);
+    }
+}
